03f4fd79cf265866e268b5b632535d6227a708b6
vigneshGDW <VigneshGDW@gmail.com>
2025-06-07 11:09:24 +0530

    Effects

--- Assets/BallGames/Scripts/BucketScript/BallCollect.cs
+++ Assets/BallGames/Scripts/BucketScript/BallCollect.cs
@@ -4,11 +4,17 @@ namespace BucketMove.Level_1
 {
     public class BallCollect : MonoBehaviour
     {
+        public int count = 0;
         private void OnCollisionExit2D(Collision2D other)
         {
             if (other.gameObject != null)
             {
                 other.gameObject.SetActive(false);
+                count += 1;
+                if (count == 25)
+                {
+                    Debug.Log("Level 1 - Completed");
+                }
             }
         }
     }
